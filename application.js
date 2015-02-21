9ef8f2ebdc176d9f8d64cb07057275c468eaac25
Rocky Tang <rocky.tang168@gmail.com>
2015-02-21 13:03:30 -0800

    WIP individual user tweets

--- application.js
+++ application.js
@@ -1,4 +1,70 @@
 
+/* $(document).ready(function(){
+  //var $body = $('body');
+  //$body.html('');
+  var $section = $('section');
+  $section.html('');
+
+  var index = streams.home.length - 1;
+
+  while(index >= 0){
+    var tweet = streams.home[index];
+    var $tweet = $('<div class="tweets"></div>');
+
+    $tweet.html( "<p>" + 
+      "<a href=streams.users[tweet.user]>" + '@' + tweet.user + "</a>" + ': ' + tweet.message +
+      "<br> <span class='postedTime'> Posted on: " + timeStamp(time) + "</span> </p>");
+//streams.users.shawndrost has all of shawndrost's tweets
+    $tweet.appendTo($section);
+    index -= 1;
+  }  
+}); */
+
+$(document).ready(function(){
+  //var $body = $('body');
+  //$body.html('');
+  var $section = $('section');
+  $section.html('');
+
+  var index = streams.users.shawndrost.length - 1;
+
+  while(index >= 0){
+    var tweet = streams.users.shawndrost[index];
+    var $tweet = $('<div class="tweets"></div>');
+
+    $tweet.html( "<p>" + 
+      "<a href=streams.users[tweet.user]>" + '@' + tweet.user + "</a>" + ': ' + tweet.message +
+      "<br> <span class='postedTime'> Posted on: " + timeStamp(time) + "</span> </p>");
+//streams.users.shawndrost has all of shawndrost's tweets
+    $tweet.appendTo($section);
+    index -= 1;
+  }  
+});
+
+var addNewestTweets = function(){
+    setInterval(function(){
+      var $body = $('body');
+      //$body.html('');
+      var index = updatedIndex - 1;
+
+      var updatedIndex = streams.home.length - 1;
+
+      while(updatedIndex > index) {
+        var tweet = streams.home[updatedIndex];
+        var $tweet = $('<div class="tweets"></div>');
+        $tweet.html('@' + tweet.user + ': ' + tweet.message +
+          " Posted on: " + timeStamp(time));
+        $tweet.prependTo($body);
+        index -= 1;
+      };
+      index = updatedIndex;
+    }, 2000);
+  };
+
+//addNewestTweets();
+//to add back later
+
+
 var time = Date.now();
 
 /*
@@ -7,65 +73,22 @@ var time = Date.now();
 */
  
 function timeStamp() {
-// Create a date object with the current time
   var now = new Date();
- 
-// Create an array with the current month, day and time
   var date = [ now.getMonth() + 1, now.getDate() ];
- 
-// Create an array with the current hour, minute and second
   var time = [ now.getHours(), now.getMinutes(), now.getSeconds() ];
- 
-// Determine AM or PM suffix based on the hour
   var suffix = ( time[0] < 12 ) ? "AM" : "PM";
- 
-// Convert hour from military time
+
   time[0] = ( time[0] < 12 ) ? time[0] : time[0] - 12;
- 
-// If hour is 0, set it to 12
+
   time[0] = time[0] || 12;
  
-// If seconds and minutes are less than 10, add a zero
   for ( var i = 1; i < 3; i++ ) {
     if ( time[i] < 10 ) {
       time[i] = "0" + time[i];
     }
   }
- 
-// Return the formatted string
+
   return date.join("/") + " " + time.join(":") + " " + suffix;
 }
 
-$(document).ready(function(){
-  var $body = $('body');
-  $body.html('');
-
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    var $tweet = $('<div class="tweets"></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message +
-      " Posted on: " + timeStamp(time));
-    $tweet.appendTo($body);
-    index -= 1;
-  }
-
-});
-
-var addNewestTweets = function(){
-  setInterval(function(){
-    var $body = $('body');
-    $body.html('');
-
-    var index = streams.home.length - 1;
-    while(index >= 0){
-      var tweet = streams.home[index];
-      var $tweet = $('<div class="tweets"></div>');
-      $tweet.text('@' + tweet.user + ': ' + tweet.message +
-        " Posted on: " + timeStamp(time));
-      $tweet.appendTo($body);
-      index -= 1;
-    }
-  }, 8000);
-};
-addNewestTweets();
+  
